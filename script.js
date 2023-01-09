9b20399b25a5d8162578fffcf381925626339e75
Frederic Carboneras <carboneras.frederic@gmail.com>
2023-01-09 18:03:27 +0100

    changed many things

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let elfes = [
-    
-]
+import confetti from "./node_modules/canvas-confetti";
+
+const button = document.querySelector("#btn-party");
+
+button.addEventListener("click", ($event) => {
+    const target = $event.target.getBoundingClientRect();
+    const halfWidth = target.width / 2;
+    const halfHeight = target.height / 2;
+
+    confetti ({
+        particleCount: 150,
+        spread: 60,
+        color: ["#00fff1", "ff00aa", "#46fcb4"],
+        origin: {
+            X: (target.x + halfWidth) / window.innerWidth,
+            y: (target.x + halfHeight) / window.innerHeight,
+        }
+    })
+});

959c986df665973cc9025eb4c5da60f85980c65a
Frederic Carboneras <carboneras.frederic@gmail.com>
2023-01-10 15:06:28 +0100

    deletet npm canvas confetti and added it as CDN
    the NPM Package Canvas_Confetti doesn't work without Webpack, so i deleted it, and added it as CDN.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-import confetti from "./node_modules/canvas-confetti";
-
 const button = document.querySelector("#btn-party");
 
 button.addEventListener("click", ($event) => {
     const target = $event.target.getBoundingClientRect();
+    console.log(target);
     const halfWidth = target.width / 2;
     const halfHeight = target.height / 2;
 
@@ -13,7 +12,7 @@ button.addEventListener("click", ($event) => {
         color: ["#00fff1", "ff00aa", "#46fcb4"],
         origin: {
             X: (target.x + halfWidth) / window.innerWidth,
-            y: (target.x + halfHeight) / window.innerHeight,
+            y: (target.y + halfHeight) / window.innerHeight,
         }
     })
 });
